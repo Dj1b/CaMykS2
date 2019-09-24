5a817b011d60c6cc8e33676664d88dcf53e17282
JB Lebrun <jb@ideogram-design.fr>
2019-09-24 16:50:56 +0200

    Enhances CaMykS master library.

--- CaMykS.php
+++ CaMykS.php
@@ -1,42 +1,45 @@
 <?php
-/*
- * CaMykS Engine
- * Developed by		: camyks.net
- * Author		    : CaMykS Team <camyks.contact@gmail.com>
- * Object Version	: 2.0a
- * Object Type      : Engine / Main Object
- * Creation Date    : Feb 2017
- * Last Modif Date  : Feb 2017
- *
- * CaMykS engine object
+/**
+ * @brief CaMykS version 2 CMS master class.
+ * @details Engine / Main Object
+ * @file CaMykS.php
+ * @author CaMykS Team <camyks.contact@gmail.com>
+ * @version 2.0a
+ * @date Creation: Feb 2017
+ * @date Modification: Sep 2019
+ * @copyright 2017 - 2019 CaMykS Team
+ * @note This program is distributed as is - WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
- 
-class CaMykS {
- 
-   
-  /*
-   * constructor
-   * @return void
-   * @access public
-   */
-  public __construct() {
-   
-  }
-   
-  /*
-   * run CaMykS engine
-   * @param integer $mode to run
-   * @return void
-   * @access public
-   */
-  public run($mode) {
-  
-  }
+
+/**
+ * AdminoTemplateMaster class.
+ */
+final class CaMykS {
+
+
+    /**
+     * Class constructor.
+     * @return void
+     */
+    public function __construct() {
+
+    }
+
+    /**
+     * Execute CaMykS engine
+     * @param integer $mode to run
+     * @return void
+     */
+    public function execute($mode) {
+
+    }
 }
 
 /* check for manager execution */
 if ($_REQUEST['SCRIPT_FILENAME'] === 'CaMykS.php') {
-  $CaMykS = new CaMykS();
-  /* execute CaMykS engine */
-  $CaMykS->execute('manager');
+    $CaMykS = new CaMykS();
+    /* execute CaMykS engine */
+    $CaMykS->execute('manager');
 }
+?>
