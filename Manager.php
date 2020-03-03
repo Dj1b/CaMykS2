ec9562e1438d883f4c14aaf6b82d52001f4d8d69
JB Lebrun <jb@ideogram-design.fr>
2020-03-03 18:17:20 +0100

    Enhances manager entry point.

--- Manager.php
+++ Manager.php
@@ -3,10 +3,10 @@
  * @brief Manager entry point for CaMykS2.
  * @details Engine / Index Page
  * @author CaMykS Team <camyks.contact@gmail.com>
- * @version 2.0a1
+ * @version 2.0a2
  * @date Creation: Feb 2017
- * @date Modification: Nov 2019
- * @copyright 2017 - 2019 CaMykS Team
+ * @date Modification: Mar 2020
+ * @copyright 2017 - 2020 CaMykS Team
  * @note This program is distributed as is - WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
@@ -16,10 +16,10 @@ session_start();
 
 /* Load CaMykS */
 if ((@include_once('CaMykS.php.inc')) === false)
-    exit('Fatal Error : CaMykS folder not found.');
+    exit('Fatal Error : CaMykS engine not found.');
 
 /* Create CaMykS engine object */
-$CaMykS = new CaMykS2\CaMykS(null);
+$CaMykS = new CaMykS2\CaMykS();
 
 /* Execute CaMykS engine */
 $CaMykS->execute(CAMYKS_MODE_MANAGER);
