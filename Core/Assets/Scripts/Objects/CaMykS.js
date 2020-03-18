60212e156f1b6afae3ac415051b3d9ec1e988e29
Dj1b <skymac.contact@gmail.com>
2020-03-18 15:43:09 +0100

    Inserts first draft of CaMykS object for client side scripts.

---
+++ Core/Assets/Scripts/Objects/CaMykS.js
@@ -0,0 +1,74 @@
+/**
+ * @brief CaMykS client side master script
+ * @details Engine / Javascript
+ * @author CaMykS Team
+ * @version 1.0.0
+ * @date Creation: Mar 2020
+ * @date Modification: Mar 2020
+ * @copyright 2020 Ideogram Design
+ */
+var CaMykS = {
+    name: 'CaMykS',
+    loaded: false,
+    params: {},
+    locales: {},
+
+    /**
+     * Set parameter.
+     * @param string name
+     * @param mixed value
+     * @return void
+     */
+    set_param: function(param, value, subvalue) {
+        if (subvalue != undefined && this.params[param])
+            this.params[param][value] = subvalue;
+        else
+            this.params[param] = value;
+    },
+
+    /**
+     * Return param value from name
+     * @param mixed param
+     * @return mixed
+     */
+    get_param: function(param, value) {
+        if (value != undefined)
+            if (this.params[param][value] || this.params[param][value] === 0 || this.params[param][value] === false)
+                return this.params[param][value];
+            else
+                return false;
+        if (this.params[param])
+            return this.params[param];
+        return false;
+    },
+
+    /**
+     * Add locale
+     * @param string name
+     * @param mixed value
+     * @return void
+     */
+    add_locale: function(name, value) {
+        this.locales[name] = value;
+    },
+
+    /**
+     * return locale value from name
+     * @param mixed param
+     * @return mixed
+     */
+    get_locale: function(name) {
+        if (this.locales[name])
+            return this.locales[name];
+        return name;
+    },
+
+    /**
+     * Initialise object
+     * @return void
+     */
+    initialise: function() {
+        /* Set object as loaded */
+        this.loaded = true;
+    }
+}
