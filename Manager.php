c699ffe87fd0a587d1cd5506e8bb7d226ed0b65b
JB Lebrun <jb@ideogram-design.fr>
2021-05-15 16:04:51 +0200

    Enhances PHP session management.

--- Manager.php
+++ Manager.php
@@ -3,17 +3,13 @@
  * @brief Manager entry point for CaMykS2.
  * @details Engine / Index Page
  * @author CaMykS Team <camyks.contact@gmail.com>
- * @version 2.0a2
+ * @version 1.0pre3
  * @date Creation: Feb 2017
- * @date Modification: Mar 2020
- * @copyright 2017 - 2020 CaMykS Team
+ * @date Modification: May 2021
+ * @copyright 2017 - 2021 CaMykS Team
  * @note This program is distributed as is - WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
-
-/* Start session */
-session_start();
-
 /* Load CaMykS */
 if ((@include_once('CaMykS.php.inc')) === false)
     exit('Fatal Error : CaMykS engine not found.');
@@ -23,5 +19,4 @@ $CaMykS = new CaMykS2\CaMykS();
 
 /* Execute CaMykS engine */
 $CaMykS->execute(CAMYKS_MODE_MANAGER);
-
 ?>
